c82a74651e13fbe81e6a86ec79a177950f1e473c
kgr718 <122556068+kgr718@users.noreply.github.com>
2023-03-27 02:56:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hammer = document.querySelector(".hammer");
 const portals = [...document.querySelectorAll(".portal")];
 const scoreIs1 = document.querySelector(".score span");
-let score = 0
+let score = 0// starting point for the code
 
 function run(){
     const i = Math.floor(Math.random() * portals.length);
@@ -10,7 +10,7 @@ function run(){
     const img = document.createElement("img");
     img.classList.add("bakumon");
     img.src = "images/bakumonW.jpeg";
-    
+    // this is where the magic happens, created the image tag here and also programmed it to jump from portal to portal
 
     img.addEventListener("click", () => {
             score += 1;
@@ -21,9 +21,9 @@ function run(){
                 portal.removeChild(img)
                 run()
             }, 500)
-    })
+    }) //this code adds a click event listener that incriments the score plus 1 and also changes the image upon click
 
-    portal.appendChild(img)
+    portal.appendChild(img) //appending is what allows it to render
 
     timer = setTimeout(() => {
         portal.removeChild(img)
@@ -39,11 +39,13 @@ run()
 window.addEventListener("mousemove", e => {
     hammer.style.top = e.pageY + "px"
     hammer.style.left = e.pageX + "px"
-})
+}) // this is what allows zudomon to latch on to the the cursor, i didnt take off the cursor because the cursor click is what the bakumon reacts to, not necessarily the hammer
 
 window.addEventListener("mousedown", () =>{
-    hammer.classList.add("active")
+    hammer.classList.add("active") //when i click down bakumon gets hit with the ZUUUUDOOOOHAMMERRR
 }) 
 window.addEventListener("mouseup", ()=>{
-    hammer.classList.remove("active")
+    hammer.classList.remove("active") //when i release, the digimon goes back up
 })
+
+// https://www.youtube.com/watch?v=YPLK2SPCl90
